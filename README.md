4837e1ca8fb729870b0b991503c49d97cbf19482
Markus Ast <m@rkusa.st>
2022-01-25 16:07:42 +0100

    add /query and /execute endpoints

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
 # `do-sqlite`
 
-[Experimental] Persist SQLite in a Cloudflare Durable Object.
+[Experimental] POC that persists SQLite in a Cloudflare Durable Object.
 
-https://ma.rkusa.st/store-sqlite-in-cloudflare-durable-objects
+My journey of creating it: https://ma.rkusa.st/store-sqlite-in-cloudflare-durable-objects
+
+## Next Steps
+
+- [ ] Add support for persistent journal files (to prevent data loss and database corruption)
+- [ ] Add proper error handling
+- [ ] Ensure that everything works fine when the DO is used concurrently
+
+## Usage
+
+Deploy to Cloudflare:
 
 ```bash
-wrangler dev
 wrangler publish
+```
+
+Execute a query (path pattern: `/:database/{query,execute}`):
+
+```bash
+curl -i -X POST -H 'Content-Type: application/json' \
+  -d '{"sql":"CREATE TABLE vals (id INTEGER PRIMARY KEY AUTOINCREMENT, val VARCHAR NOT NULL)"}' \
+  https://do-sqlite.YOUR_WORKERS.workers.dev/main/execute
+```
+
+```bash
+curl -i -X POST -H 'Content-Type: application/json' \
+  -d '{"sql":"INSERT INTO vals (val) VALUES (?1)","params":["val"]}' \
+  https://do-sqlite.YOUR_WORKERS.workers.dev/main/execute
+```
+
+```bash
+curl -i -X POST -H 'Content-Type: application/json' \
+  -d '{"sql":"SELECT * FROM vals"}' \
+  https://do-sqlite.YOUR_WORKERS.workers.dev/main/query
 ```
--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,4 @@
-import Sqlite from "@rkusa/wasm-sqlite";
+import Sqlite, { Param } from "@rkusa/wasm-sqlite";
 
 interface Env {
   DATABASE: DurableObjectNamespace;
@@ -13,10 +13,63 @@ export class Database {
 
   pages: Array<Uint8Array> = [];
 
-  async fetch(_req: Request) {
-    // const query: { sql: string; params: Array<Param> } = await req.json();
-    const storage = this.state.storage;
+  async fetch(req: Request) {
+    if (req.method !== "POST") {
+      return new Response("only POST requests are allowed", {
+        status: 405 /* method not allowed */,
+      });
+    }
 
+    const url = new URL(req.url);
+    let isQuery = false;
+    if (url.pathname === "/query") {
+      isQuery = true;
+    } else if (url.pathname !== "/execute") {
+      return new Response("not found", {
+        status: 404 /* not found */,
+      });
+    }
+
+    // parse body
+    const query: { sql: string; params: Array<Param> } = await req.json();
+
+    if (!query || typeof query !== "object") {
+      return new Response("expected body to be an object", {
+        status: 400 /* bad request */,
+      });
+    }
+
+    // validate sql property
+    if (typeof query?.sql !== "string") {
+      return new Response("expected `body.sql` to be a string", {
+        status: 400 /* bad request */,
+      });
+    }
+
+    // validate params property
+    if (
+      query?.params &&
+      (!Array.isArray(query?.params) ||
+        query.params.find(
+          (p) =>
+            !(
+              p === null ||
+              typeof p === "string" ||
+              typeof p === "number" ||
+              typeof p === "boolean"
+            )
+        ))
+    ) {
+      return new Response(
+        "expected `body.params` to be an array of `string | number | boolean | null`",
+        {
+          status: 400 /* bad request */,
+        }
+      );
+    }
+
+    // instantiate SQLite and plug it into the DO's storage
+    const storage = this.state.storage;
     const sqlite = await Sqlite.instantiate(
       async (ix: number) => {
         const page: Array<number> =
@@ -28,36 +81,43 @@ export class Database {
       }
     );
 
-    // const json = await sqlite.queryRaw(query.sql, query.params);
-    // return new Response(json, {
-    //   headers: {
-    //     "content-type": "application/json; charset=utf-8",
-    //   },
-    // });
-
-    await sqlite.execute(
-      "CREATE TABLE IF NOT EXISTS vals (id INT PRIMARY KEY, val VARCHAR NOT NULL)",
-      []
-    );
-    await sqlite.execute("INSERT INTO vals (val) VALUES (?1)", ["val"]);
-    const result: Array<{ count: number }> = await sqlite.query(
-      "SELECT COUNT(*) AS count FROM vals",
-      []
-    );
-    const count = result[0].count;
-
-    return new Response(`Row Count: ${count}`);
+    if (isQuery) {
+      const json = await sqlite.queryRaw(query.sql, query.params);
+      return new Response(json, {
+        headers: {
+          "content-type": "application/json; charset=utf-8",
+        },
+      });
+    } else {
+      await sqlite.execute(query.sql, query.params);
+      return new Response(null, {
+        status: 204 /* no content */,
+      });
+    }
   }
 }
 
 export default {
   async fetch(req: Request, env: Env) {
-    if (req.method !== "GET") {
+    if (req.method !== "POST") {
       return new Response(null, { status: 405 }); // method not allowed
     }
 
-    const id = env.DATABASE.idFromName("sqlite");
+    // Expected pattern: /:database/{query,execute}
+    const url = new URL(req.url);
+    const segments = url.pathname.slice(1).split("/");
+    if (segments.length !== 2) {
+      return new Response("not found", {
+        status: 404 /* not found */,
+      });
+    }
+    const [name, path] = segments;
+
+    const id = env.DATABASE.idFromName(name);
     const stub = env.DATABASE.get(id);
-    return stub.fetch("http://sqlite/");
+    return stub.fetch(`http://sqlite/${path}`, {
+      method: "POST",
+      body: req.body,
+    });
   },
 };
